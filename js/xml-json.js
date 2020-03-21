098546f3d9630f72266dd4801c4156ab9e4aa8e5
Austin <Austin_MacBook@austins-mbp.attlocal.net>
2020-03-21 15:09:23 -0500

    function creates new JSON file to manipulate

--- js/xml-json.js
+++ js/xml-json.js
@@ -18,80 +18,85 @@
 const fs = require('fs'), xml2js = require('xml2js')
 const parser = new xml2js.Parser()
 
-fs.readFile(`${__dirname}/../data/Google_Product_Feed_File_Yogi_V7.0.xml`.toString(),
-    function(err, data) {
-      parser.parseString(data, function(err, result) {
-        console.log(JSON.stringify(result, null, 4))
-        // console.log(result)
-        // console.log(util.inspect(result, false, null))
-      })
-    })
+// fs.readFile(`${__dirname}/../data/Google_Product_Feed_File_Yogi_V7.0.xml`.toString(),
+//     function(err, data) {
+//       parser.parseString(data, function(err, result) {
+//         console.log(JSON.stringify(result, null, 4))
+//         // console.log(result)
+//         // console.log(util.inspect(result, false, null))
+//       })
+//     })
 
 
 // Separate method that's more specific without using library - needs cleaning
-// const run = () => {
-//   let file = fs.readFileSync(__dirname + '/data/Google_Product_Feed_File_Yogi_V7.0.xml').toString()
-//   const itemArr = file.split('<item>')
-//   let product = []
-//   for(i=0; i < itemArr.length; i++){
-//     if(itemArr[i]){
-//       let itemObj = {}
-//       let itemChild = itemArr[i].split(/[<>]+/)
-//       for(j=0; j<itemChild.length; j++){
-//         if(itemChild[j] === 'g:id'){
-//           itemObj.id = itemChild[(j + 1)]
-//         }
-//         if(itemChild[j] === 'g:title'){
-//           itemObj.title = itemChild[(j + 1)];
-//         }
-//         if(itemChild[j] === 'g:description'){
-//           let descArr = itemChild[(j + 1)].split([' ']);
-//           let descStr = descArr.join(' ');
-//           let firstClean = descStr.replace(/(\r\n|\n|\r)/gm, " ");
-//           let secondClean = firstClean.split(' ');
-//           let cleanArr = [];
-//           for(v=0; v<secondClean.length; v++){
-//             if(secondClean[v - 1] === '' && secondClean[v] === ''){
-//               null;
-//             } else {
-//               cleanArr.push(secondClean[v]);
-//             }
-//           }
-//           itemObj.description = cleanArr.join(' ');
-//         }
-//         if(itemChild[j] === 'g:link'){
-//           itemObj.link = itemChild[(j + 1)];
-//         }
-//         if(itemChild[j] === 'g:image_link'){
-//           itemObj.image_link = itemChild[(j + 1)];
-//         }
-//         if(itemChild[j] === 'g:availability'){
-//           itemObj.availability = itemChild[(j + 1)];
-//         }
-//         if(itemChild[j] === 'g:price'){
-//           itemObj.price = itemChild[(j + 1)];
-//         }
-//         if(itemChild[j] === '/g:google_product_category'){
-//           itemObj.google_product_category = {};
-//         }
-//         if(itemChild[j] === 'g:product_type'){
-//           //eventually make a method to split str to array
-//           itemObj.google_product_category.product_type = itemChild[(j + 1)];
-//         }
-//         if(itemChild[j] === 'g:gtin'){
-//           itemObj.gtin = itemChild[(j + 1)];
-//         }
-//         if(itemChild[j] === 'g:item_group_id'){
-//           itemObj.item_group_id = itemChild[(j + 1)];
-//         }
-//         if(itemChild[j] === 'g:shipping_weight'){
-//           itemObj.shipping_weight = itemChild[(j + 1)];
-//         }
-//       }
-//       product.push(itemObj);
-//     }
-//   }
-//   return product;
-// }
+const run = () => {
+  let file = fs.readFileSync(__dirname + '/../data/Google_Product_Feed_File_Yogi_V7.0.xml').toString()
+  const itemArr = file.split('<item>')
+  let product = []
+  
+  for (let i = 0; i < itemArr.length; i++) {
+    if (itemArr[i]) {
+      let itemObj = {}
+      let itemChild = itemArr[i].split(/[<>]+/)
+      
+      for (let j = 0; j < itemChild.length; j++) {
+        if (itemChild[j] === 'g:id') {
+          itemObj.id = itemChild[(j + 1)]
+        }
+        if (itemChild[j] === 'g:title') {
+          itemObj.title = itemChild[(j + 1)]
+        }
+        if (itemChild[j] === 'g:description') {
+          let descArr = itemChild[(j + 1)].split([' '])
+          let descStr = descArr.join(' ')
+          let firstClean = descStr.replace(/(\r\n|\n|\r)/gm, " ")
+          let secondClean = firstClean.split(' ')
+          let cleanArr = []
+          for (let v = 0; v < secondClean.length; v++) {
+            if (secondClean[v - 1] === '' && secondClean[v] === '') {
+              null
+            }
+            else {
+              cleanArr.push(secondClean[v])
+            }
+          }
+          itemObj.description = cleanArr.join(' ')
+        }
+        if (itemChild[j] === 'g:link') {
+          itemObj.link = itemChild[(j + 1)]
+        }
+        if (itemChild[j] === 'g:image_link') {
+          itemObj.image_link = itemChild[(j + 1)]
+        }
+        if (itemChild[j] === 'g:availability') {
+          itemObj.availability = itemChild[(j + 1)]
+        }
+        // if (itemChild[j] === 'g:price') {
+        //   itemObj.price = itemChild[(j + 1)]
+        // }
+        if (itemChild[j] === '/g:google_product_category') {
+          itemObj.google_product_category = {}
+        }
+        if (itemChild[j] === 'g:product_type') {
+          itemObj.google_product_category.product_type = itemChild[(j + 1)]
+        }
+        // if (itemChild[j] === 'g:gtin') {
+        //   itemObj.gtin = itemChild[(j + 1)]
+        // }
+        if (itemChild[j] === 'g:item_group_id') {
+          itemObj.item_group_id = itemChild[(j + 1)]
+        }
+        if (itemChild[j] === 'g:shipping_weight') {
+          itemObj.shipping_weight = itemChild[(j + 1)]
+        }
+      }
+      product.push(itemObj)
+    }
+  }
+  return product
+}
 
-// console.log(run())
+fs.writeFile('newJsonFile.json', JSON.stringify(run()), function(err, result) {
+  if (err) console.log('error', err)
+})
+// console.timeEnd()
