837d765c3e3991d58a8422066e22c8f3f839ad2d
Austin <Austin_MacBook@austins-mbp.attlocal.net>
2020-03-21 00:56:31 -0500

    data shows up in console. Needs formatting

--- js/xml-json.js
+++ js/xml-json.js
@@ -1,4 +1,4 @@
-// const myJson = {
+// const jsonData = {
 //   "item": {
 //     "id": null,
 //     "title": null,
@@ -9,22 +9,21 @@
 //     "price": null,
 //     "google_product_category": null,
 //     "product_type": null,
-//     "gtin": null,
-//     "item_group_id": null,
+//     "identifier_exists": null,
 //     "shipping_weight": null,
 //   }
 // }
 
+
 const fs = require('fs'), xml2js = require('xml2js')
 const parser = new xml2js.Parser()
 
-fs.readFile(__dirname + '../data/Google_Product_Feed_File_Yogi_V7.0.xml',
+fs.readFile(`${__dirname}/../data/Google_Product_Feed_File_Yogi_V7.0.xml`.toString(),
     function(err, data) {
       parser.parseString(data, function(err, result) {
-        console.log(JSON.stringify(result))
-        // result ? console.log(true, result) : console.log(false)
-        // console.log(err)
-        console.log('DONE')
+        console.log(JSON.stringify())
+        // console.log(result)
+        // console.log(util.inspect(result, false, null))
       })
-    }
-  )
+    })
+
